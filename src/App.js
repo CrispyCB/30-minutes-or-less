a38507a9290c30b63fa06041ee3fd871f3289412
Christian Bahnweg <cebahnweg@gmail.com>
2019-03-21 19:45:52 -0400

    creates Header and sets in App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from "../src/components/Header/Header.js"
 import './App.css';
 
 class App extends Component {
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import "./Header.css"
+
+class Header extends Component {
+    render () {
+        return(
+            <div>
+                <h1>30 Minutes or Less</h1>
+                <p>For tasks that take 30 minutes or less</p>
+            </div>
+        )
+    }
+}
+
+export default Header
