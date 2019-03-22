d7e0522d23b5f2168b31dc0169ab10e9ebeb9ae9
Christian Bahnweg <cebahnweg@gmail.com>
2019-03-21 20:07:38 -0400

    sets up TodoContainer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from "../src/components/Header/Header.js"
+import TodoContainer from "../src/components/TodoContainer/TodoContainer.js"
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
+        <TodoContainer/>
       </div>
     );
   }
